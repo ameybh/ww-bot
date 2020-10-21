efd6478f2461634b72f628d39697e006866ccec8
Amey Bhavsar <iamamey24@gmail.com>
2020-10-21 19:06:00 +0530

    Intial commit with working Hello example

---
+++ README.md
@@ -0,0 +1,26 @@
+# Foobar
+
+Boilerplate code for [whatsapp-web.js](https://github.com/pedroslopez/whatsapp-web.js) library.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to setup ww-bot.
+
+1. Clone the repository.
+```bash
+git clone https://github.com/ameybhavsar24/ww-bot.git
+```
+
+2. Install dependencies.
+```bash
+npm i
+```
+
+## Usage
+### Start the project locally.
+```bash
+npm start
+```
+
+## License
+[ISC](https://choosealicense.com/licenses/isc/)
---
+++ index.js
@@ -0,0 +1,21 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    console.log(message);
+    if (message.body.indexOf('Hi') != -1 || message.body.indexOf('Hello') != -1) {
+        message.reply('Hello there!\nRegards from ameyBot');
+    }
+});
+
+client.initialize();
