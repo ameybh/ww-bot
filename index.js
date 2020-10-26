d20832e4026236f0ae5bd9a98c9e399d901249a6
Amey Bhavsar <iamamey24@gmail.com>
2020-10-26 17:22:27 +0530

    added puppeteer args

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const puppeteerOptions = {
+    headless: true,
+    args: ["--no-sandbox"]
+};
+const client = new Client({puppeteer: puppeteerOptions});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
