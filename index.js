0e1028b6c6e70a762d4aded1b9f687f18deac904
Amey Bhavsar <iamamey24@gmail.com>
2020-10-26 17:06:25 +0530

    sandbox back again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
-const client = new Client({ args: ['--no-sandbox'] });
+const client = new Client();
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
