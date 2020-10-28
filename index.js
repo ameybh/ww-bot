a59c09545ffa5452322c4789a560c21bbff359d3
Amey Bhavsar <iamamey24@gmail.com>
2020-10-28 14:54:03 +0530

    added session functionality

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
 const puppeteerOptions = {
     headless: true,
     args: ["--no-sandbox"]
 };
-const client = new Client({puppeteer: puppeteerOptions});
+const client = new Client(
+    {
+        puppeteer: puppeteerOptions,
+        session: sessionData
+    }
+);
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
+});
+
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
 
 client.on('ready', () => {
